ffdde2b86d4dce305085b995c1c2bdd084ee7045
mvpaez2002 <mvpaez@uninorte.edu.co>
2023-11-26 18:31:33 -0500

    version 2

--- lib/ui/widgets/maps_page.dart
+++ lib/ui/widgets/maps_page.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-// ignore: constant_identifier_names
 const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1IjoicGl0bWFjIiwiYSI6ImNsY3BpeWxuczJhOTEzbnBlaW5vcnNwNzMifQ.ncTzM4bW-jpq-hUFutnR1g';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
@@ -16,6 +18,10 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   LatLng? myPosition;
+  LatLng? fixedPosition;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<Position> determinePosition() async {
     LocationPermission permission;
@@ -37,10 +43,51 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
+  Future<void> getFixedLocation() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        DocumentSnapshot snapshot =
+            await _firestore.collection('usuarios').doc(user.uid).get();
+
+        if (snapshot.exists) {
+          setState(() {
+            fixedPosition = LatLng(
+              snapshot['latitudFija'],
+              snapshot['longitudFija'],
+            );
+          });
+        }
+      }
+    } catch (e) {
+      print('Error al obtener la ubicación fija: $e');
+    }
+  }
+
+  void fixLocation(LatLng location) async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await _firestore.collection('usuarios').doc(user.uid).set({
+          'latitudFija': location.latitude,
+          'longitudFija': location.longitude,
+        });
+        setState(() {
+          fixedPosition = location;
+        });
+      }
+    } catch (e) {
+      print('Error al fijar la ubicación: $e');
+    }
+  }
+
   @override
   void initState() {
-    getCurrentLocation();
     super.initState();
+    Firebase.initializeApp().whenComplete(() {
+      getCurrentLocation();
+      getFixedLocation();
+    });
   }
 
   @override
@@ -55,14 +102,18 @@ class _MapScreenState extends State<MapScreen> {
           ? const CircularProgressIndicator()
           : FlutterMap(
               options: MapOptions(
-                  center: myPosition, minZoom: 5, maxZoom: 25, zoom: 18),
+                center: myPosition,
+                minZoom: 5,
+                maxZoom: 25,
+                zoom: 18,
+              ),
               nonRotatedChildren: [
                 TileLayer(
                   urlTemplate:
                       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
                   additionalOptions: const {
                     'accessToken': MAPBOX_ACCESS_TOKEN,
-                    'id': 'mapbox/streets-v12'
+                    'id': 'mapbox/streets-v12',
                   },
                 ),
                 MarkerLayer(
@@ -78,11 +129,55 @@ class _MapScreenState extends State<MapScreen> {
                           ),
                         );
                       },
-                    )
+                    ),
+                    if (fixedPosition != null)
+                      Marker(
+                        point: fixedPosition!,
+                        builder: (context) {
+                          return Container(
+                            child: const Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                              size: 40,
+                            ),
+                          );
+                        },
+                      ),
                   ],
                 )
               ],
             ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Fijar ubicación'),
+                content: Text(
+                  '¿Desea fijar la ubicación actual en el mapa?',
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancelar'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      fixLocation(myPosition!);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Aceptar'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add_location),
+      ),
     );
   }
 }
